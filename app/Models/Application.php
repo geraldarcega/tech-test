c3815299599c20765b3c1074a7bdf9329f564885
Gerald Arcega <michaelgeraldarcega@gmail.com>
2025-12-11 20:06:04 +0800

    add relationships

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use App\Enums\ApplicationStatus;
 use App\Events\ApplicationCreated;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Application extends Model
 {
@@ -18,4 +20,19 @@ class Application extends Model
     protected $dispatchesEvents = [
         'created' => ApplicationCreated::class,
     ];
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function plan(): BelongsTo
+    {
+        return $this->belongsTo(Plan::class);
+    }
+
+    public function scopeOrderReady($query): Builder
+    {
+        return $query->where('status', ApplicationStatus::Order);
+    }
 }
--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Plan extends Model
 {
     use HasFactory;
+
+    public function application(): BelongsTo
+    {
+        return $this->belongsTo(Application::class);
+    }
 }
